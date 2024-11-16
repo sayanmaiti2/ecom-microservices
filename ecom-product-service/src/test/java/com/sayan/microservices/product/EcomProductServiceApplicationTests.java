172f3f019d75700ace5c15713dadb16f74664d9a
Sayan Maiti <sayanmaiti31@gmail.com>
2024-11-16 22:47:02 +0530

    feat(product-service): added integration tests.

--- ecom-product-service/src/test/java/com/sayan/microservices/product/EcomProductServiceApplicationTests.java
+++ ecom-product-service/src/test/java/com/sayan/microservices/product/EcomProductServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.sayan.microservices.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EcomProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ServiceConnection
+	static MongoDBContainer mongoDbContainer = new MongoDBContainer("mongo:8.0.3");
+
+	@LocalServerPort
+	private Integer serverPort;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = serverPort;
 	}
+
+	static {
+		mongoDbContainer.start();
+	}
+
+
+	@Test
+	void shouldCreateProduct() {
+		String requestBody = """
+							{
+							  "name": "iPhone 13",
+							  "description": "Apple iPhone from 2022",
+							  "price": 1500
+							}
+				""";
+
+		RestAssured.given()
+				.body(requestBody)
+				.contentType("application/json")
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 13"))
+				.body("description", Matchers.equalTo("Apple iPhone from 2022"))
+				.body("price", Matchers.equalTo(1500));
+			}
 
 }

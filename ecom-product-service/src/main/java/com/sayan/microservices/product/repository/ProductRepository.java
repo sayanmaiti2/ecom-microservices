9f16b8eef4b9d3a49fa27600bb79c0fbfad4b52c
Sayan Maiti <sayanmaiti31@gmail.com>
2024-11-16 21:35:42 +0530

    feat(product-service): added Product Microservice implementation.

---
+++ ecom-product-service/src/main/java/com/sayan/microservices/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sayan.microservices.product.repository;
+
+import com.sayan.microservices.product.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
---
+++ ecom-product-service/src/main/java/com/sayan/microservices/product/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.sayan.microservices.product.service;
+
+import com.sayan.microservices.product.dto.ProductRequest;
+import com.sayan.microservices.product.dto.ProductResponse;
+import com.sayan.microservices.product.model.Product;
+import com.sayan.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .id(productRequest.id())
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product created successfully with ID: {}", product.getId());
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice())).toList();
+    }
+}

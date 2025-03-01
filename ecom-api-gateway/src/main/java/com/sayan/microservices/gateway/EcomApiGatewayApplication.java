ca880e8286c06abcb709f0f81dac679539aaf4fb
Sayan Maiti <sayanmaiti31@gmail.com>
2025-03-01 17:38:18 +0530

    feat(api-gateway): Initialize API Gateway with routing for product, order, and inventory services

---
+++ ecom-api-gateway/src/main/java/com/sayan/microservices/gateway/EcomApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.microservices.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApiGatewayApplication.class, args);
+	}
+
+}

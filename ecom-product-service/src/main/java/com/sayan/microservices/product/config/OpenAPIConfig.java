5267f7494ef706f7bb8801e281c672cebe4e98b4
Sayan Maiti <sayanmaiti31@gmail.com>
2025-03-02 18:31:27 +0530

    feat(product-service): Update Swagger UI configuration and API documentation paths

--- ecom-product-service/src/main/java/com/sayan/microservices/product/config/OpenAPIConfig.java
+++ ecom-product-service/src/main/java/com/sayan/microservices/product/config/OpenAPIConfig.java
@@ -14,7 +14,7 @@ public class OpenAPIConfig {
     public OpenAPI productServiceAPI() {
         return new OpenAPI()
                 .info(new Info().title("Product Service API")
-                        .description("This is the REST API for Product Service")
+                        .description("REST API for Product Service")
                         .version("v0.0.1")
                         .license(new License().name("Apache 2.0")))
                 .externalDocs(new ExternalDocumentation()

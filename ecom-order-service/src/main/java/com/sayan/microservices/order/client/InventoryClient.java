c7c24653a78a251a9a988d75ed537500cca792b5
Sayan Maiti <sayanmaiti31@gmail.com>
2025-03-03 11:36:40 +0530

    feat(inventory): Implement circuit breaker and retry mechanism for inventory checks

--- ecom-order-service/src/main/java/com/sayan/microservices/order/client/InventoryClient.java
+++ ecom-order-service/src/main/java/com/sayan/microservices/order/client/InventoryClient.java
@@ -1,10 +1,16 @@
 package com.sayan.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
     /**
      * Checks if the specified quantity of a product is in stock.
@@ -14,6 +20,13 @@ public interface InventoryClient {
      * @return true if the specified quantity is in stock, false otherwise
      */
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {} and quantity: {}, failure reason: {}", code, quantity, throwable.getMessage());
+        return false;
+    }
 
 }
--- ecom-order-service/src/main/java/com/sayan/microservices/order/config/RestClientConfig.java
+++ ecom-order-service/src/main/java/com/sayan/microservices/order/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.sayan.microservices.order.config;
 
 import com.sayan.microservices.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -23,10 +28,23 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
 
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyfactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyfactory.createClient(InventoryClient.class);
+    }
+
+    /**
+     * Creates and configures a ClientHttpRequestFactory with custom timeout settings.
+     *
+     * @return an instance of ClientHttpRequestFactory
+     */
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
